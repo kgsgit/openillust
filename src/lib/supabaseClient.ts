5318280d7bc69d1a081980ee28da2433f597bdde
[Iot] 김기석 <wekgs@naver.com>
2025-07-19 17:32:04 +0900

    toml2

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// 환경 변수로부터 Supabase URL과 키를 가져옵니다.
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-// Supabase 클라이언트를 한 번만 생성하여 다른 파일에서 사용할 수 있도록 export 합니다.
+// Supabase 클라이언트를 한 번만 생성하여 다른 파일에서 사용할 수 있도록 export
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
