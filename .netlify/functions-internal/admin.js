d9cfeae80c70e79bf3769f68341dfe72b0bad776
[Iot] 김기석 <wekgs@naver.com>
2025-07-19 17:48:15 +0900

    toml2

---
+++ .netlify/functions-internal/admin.js
@@ -0,0 +1,16 @@
+// functions-internal/admin.js
+
+exports.handler = async function(event, context) {
+  if (event.httpMethod === 'POST') {
+    // 로그인 처리 로직 (여기에 로그인 처리 코드 작성___)
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'POST request received' }),
+    };
+  } else {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ message: 'Method Not Allowed' }),
+    };
+  }
+};
