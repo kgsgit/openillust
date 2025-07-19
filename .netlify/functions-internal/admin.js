1ce848789563c13ec5b31564c32551737f7088b0
[Iot] 김기석 <wekgs@naver.com>
2025-07-19 17:57:32 +0900

    toml2

--- .netlify/functions-internal/admin.js
+++ .netlify/functions-internal/admin.js
@@ -2,7 +2,7 @@
 
 exports.handler = async function(event, context) {
   if (event.httpMethod === 'POST') {
-    // 로그인 처리 로직 (여기에 로그인 처리 코드 작성___)
+    // 로그인 처리 로직 (여기에 로그인 처리 코드 작성)
     return {
       statusCode: 200,
       body: JSON.stringify({ message: 'POST request received' }),
